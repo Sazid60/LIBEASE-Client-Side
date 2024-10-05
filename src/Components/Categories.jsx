483e41a37aea13902d437524842bb8b70cc865ff
Shahnawaz Sazid <shahnawazsazid60@gmail.com>
2024-10-05 22:06:27 +0600

    Changes in categry section Design

--- src/Components/Categories.jsx
+++ src/Components/Categories.jsx
@@ -8,7 +8,7 @@ const Categories = ({ category }) => {
     // eslint-disable-next-line react/prop-types
     const { categoryName, pictureURL,description } = category
     return (
-        <div className="rounded-2xl">
+        <div className="rounded-2xl hover:-translate-y-2 duration-700">
             <div className="relative shadow-xl rounded-2xl">
                 <img
                     src={pictureURL}
@@ -18,7 +18,7 @@ const Categories = ({ category }) => {
                 <span className="absolute inset-0 flex flex-col items-center justify-center text-center text-white font-bold text-2xl md:text-3xl lg:text-4xl">
                     {categoryName}
                     <span className="font-normal text-xs md:text-sm lg:text-lg mt-2">{description}</span>
-                    <Link to={`/categorized-books/${categoryName}`}><button className="btn  btn-sm mt-4 bg-[#333333] text-white">View Books</button></Link>
+                    <Link to={`/categorized-books/${categoryName}`}><button className="btn btn-sm mt-4 text-white border border-white bg-transparent hover:bg-transparent">View Books</button></Link>
                 </span>
             </div>
         </div>
