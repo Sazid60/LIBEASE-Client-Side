422c78e49b1b6c08afd83e4144cdb11d7fa33598
Shahnawaz Sazid <shahnawazsazid60@gmail.com>
2024-10-22 07:19:19 +0600

    Libease Updated for React 18.3.1

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
 // import App from './App.jsx'
 import './index.css'
 
@@ -12,10 +12,13 @@ import AuthProvider from './AuthProvider/AuthProvider';
 
 
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
     <AuthProvider>
       <RouterProvider router={router} />
     </AuthProvider>
-  </React.StrictMode>,
+  </StrictMode>,
 )
+
+
+
